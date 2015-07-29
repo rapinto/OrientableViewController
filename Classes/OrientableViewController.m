b1739c0651e0126226547ed2439de47b91e341db
Raphael Pinto <rpinto@dealabs.com>
2015-07-29 15:23:18 +0200

    update header files

--- Classes/OrientableViewController.m
+++ Classes/OrientableViewController.m
@@ -1,16 +1,41 @@
 //
 //  OrientableViewController.m
-//  Dealabs
+//
 //
 //  Created by Raphaël Pinto on 27/07/2015.
-//  Copyright (c) 2015 HUME Network. All rights reserved.
 //
+// The MIT License (MIT)
+// Copyright (c) 2015 Raphael Pinto.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
 
 #import "OrientableViewController.h"
+
+
 
 @interface OrientableViewController ()
 
 @end
+
+
 
 @implementation OrientableViewController
 
@@ -116,8 +141,8 @@
 // iOS 7 support
 - (void)applicationDidChangeStatusBarOrientation
 {
-    float lMaxValue = MAX([OrientableViewController orientationDependentScreenSize].width, [OrientableViewController orientationDependentScreenSize].height);
-    float lMinValue = MIN([OrientableViewController orientationDependentScreenSize].width, [OrientableViewController orientationDependentScreenSize].height);
+    float lMaxValue = MAX([AppUtils orientationDependentScreenSize].width, [AppUtils orientationDependentScreenSize].height);
+    float lMinValue = MIN([AppUtils orientationDependentScreenSize].width, [AppUtils orientationDependentScreenSize].height);
     
     if ([[UIDevice currentDevice] orientation] == UIInterfaceOrientationLandscapeLeft ||
         [[UIDevice currentDevice] orientation] == UIInterfaceOrientationLandscapeRight)
